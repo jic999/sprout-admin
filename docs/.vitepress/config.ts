3366539cb6a5df8ad7cc0cd9b31b9028d507e3a5
jic999 <jic999@qq.com>
2023-10-26 23:35:12 +0800

    docs: favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Sprout Admin",
-  description: "一个开源的轻量级 RBAC 后台管理模板",
+  title: 'Sprout Admin',
+  description: '一个开源的轻量级 RBAC 后台管理模板',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.svg' }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +19,13 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
 })
